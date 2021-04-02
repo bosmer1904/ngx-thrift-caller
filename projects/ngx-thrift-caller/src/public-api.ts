90e830424c9023f6d8d89e04dc1c579daf209aee
Nikita <nikitadishlenko@gmail.com>
2021-04-02 14:41:42 +0300

    change docs

--- projects/ngx-thrift-caller/src/public-api.ts
+++ projects/ngx-thrift-caller/src/public-api.ts
@@ -5,6 +5,7 @@
 export { NgxThriftCaller } from './lib/ngx-thrift-caller.service';
 export { NgxThriftCallerModule } from './lib/ngx-thrift-caller.module';
 export { TransferHttpService } from './lib/http/transfer-http.service';
+export { TransferHttpServiceConfig } from './lib/http/transfer-http-service-config';
 export * from './lib/angular-xhr/creations';
 export { AngularXHRClient } from './lib/angular-xhr/angular-xhr-client';
 export { AngularXhrConnection } from './lib/angular-xhr/angular-xhr-connection';
